abaa62f7ef17fbf257399b3e5d76d605e1cb7ac8
bob4321at <90276384+bob4321at@users.noreply.github.com>
2022-09-26 18:52:29 -0600

    j

---
+++ cat game/Assets/movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public float maxspeed;
+    public float jumpheight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movx = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+
+        Vector2 movedir = transform.right * movx;
+
+        if (Physics2D.Raycast(transform.position, Vector2.down, 0.7f)) {
+            if (Input.GetButtonDown("Jump")) {
+                rb.AddForce(new Vector2(0, jumpheight));
+            }
+        }
+
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxspeed, maxspeed), Mathf.Clamp(rb.velocity.y, -10000, 10000));
+        rb.AddForce(movedir);
+    }
+}
