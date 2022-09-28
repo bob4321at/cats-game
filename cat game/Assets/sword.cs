dd6fb9b847d5e05d5246bdd46b46a6dcdbe22d0e
bob4321at <90276384+bob4321at@users.noreply.github.com>
2022-09-27 18:19:10 -0600

    j

--- cat game/Assets/sword.cs
+++ cat game/Assets/sword.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class sword : MonoBehaviour
 {
+    private BoxCollider2D bc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bc = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
